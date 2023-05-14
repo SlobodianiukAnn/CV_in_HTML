c0222bb20adf0bc4a622ac8338a394f34effbd8f
AnnSlobodianiuk <slobodianiukann16@gmail.com>
2023-05-14 12:15:36 +0300

    first commit

---
+++ CV.js
@@ -0,0 +1,63 @@
+<html>
+    <body>
+<header>
+    
+<h1 align = 'center'>Anna Slobodianiuk </h1>
+<p align = 'center'><strong>JavaScript Developer</strong></p>
+<p align = "center"><img src = "https://cdn2.iconfinder.com/data/icons/avatars-99/62/avatar-370-456322-512.png" width = 150px > 
+</header>
+</p>
+<hr>
+<section>
+<h2>Summary</h2>
+<p>I am a beginner JavaScript programmer. I am 22 years old, I was born in Vinnitsa. I first started programming at university. 
+    <br></br>Programming is more of a hobby of mine, but I would really like it to become a permanent part of my life.</p>
+</section>
+<section>
+<h2>Experianse</h2>
+<ul>
+  <li>
+    <b>Shop assistant in EVA Company</b> | 2018-2020
+  </li>
+   <li>
+    <b>Secretary in PlasmaTec Company</b> | 2022-2023
+  </li>
+</ul>
+</section>
+<section>
+<h2><i>Skills</i></h2>
+<table border="1">
+  <tr>
+      <th>Hard Skills</th>
+      <td>JavaScript</td>
+      <td>SQL</td>
+      <td>HTML and CSS</td>
+  </tr>
+  <tr>
+      <th>Soft Skills</th>
+      <td>Emotional intellect</td>
+      <td>Unconventional creative thinking</td>
+      <td>Empathy</td>
+  </tr>
+</table>
+</section>
+<form>
+  <h2>Contact Us</h2>
+ 
+  <label for="name">Name: </label>
+  <input type="text" id="name" placeholder = "Name"><br>
+   <label for="name">Email: </label>
+  <input type="text" id="name" placeholder = "Email"><br><br>
+  <label for="message">Message: </label><br>
+  <textarea placeholder = "Message"></textarea><br>
+  <input type="button" value="Send">
+</form>  
+
+    </body>
+
+<footer>
+  <h2>Contacts</h2>
+  <a href="https://www.linkedin.com/in/%D0%B0%D0%BD%D1%8F-%D1%81%D0%BB%D0%BE%D0%B1%D0%BE%D0%B4%D1%8F%D0%BD%D1%8E%D0%BA-083756255/" target="_blank">LinkedIn</a>
+  <p>Copyright &copy; Anna Slobodianiuk</p>
+</footer>
+</html>
